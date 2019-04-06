6337217043feae52fa9786a01b4c3a485c9e5a24
leesoo <isoo7510@gmail.com>
2019-04-06 17:32:35 +0900

    Add Heading Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import styles from './App.module.css';
+import {Heading} from './components';
 
+console.log(styles);
 class App extends Component {
   render() {
     return (
-      <div>
-        I am App
+      <div className={styles.wrapper}>
+        <Heading />
       </div>
     );
   }
